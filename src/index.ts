e758253315256d8f38b7d00365d2b79002ed3517
noah <30538387+fourwadu@users.noreply.github.com>
2023-02-07 16:32:10 -0500

    fix import?

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export { Watcher } from "./watcher";
 export { VerificationClient } from "./base";
 
 // Generated types
-export type { TRUVERIFI_SERVICES } from "../gen/services.d.ts";
+export { TRUVERIFI_SERVICES } from "../gen/services";
