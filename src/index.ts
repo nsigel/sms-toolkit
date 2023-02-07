d5756711e8207eccde206403b036b95086f35c0d
noah <30538387+fourwadu@users.noreply.github.com>
2023-02-07 16:15:09 -0500

    export genned code

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@
 export { Truverifi } from "./truverifi";
 export { Watcher } from "./watcher";
 export { VerificationClient } from "./base";
+
+// Generated types
+export { TRUVERIFI_SERVICES } from "../gen/services";
